35f8b99311fb919bc77f502195adb4d35eeaabb8
jingjieli95 <41805075+jingjieli95@users.noreply.github.com>
2024-10-31 15:37:28 +0000

    message

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -9,55 +9,50 @@ const Hero = () => (
   <Section>
     <HeroAvatar
       title={
-        <>
-          Hi there, I'm <GradientText>Jingjie</GradientText> 👋
-        </>
+
+        <div class="relative mt-5 mb-0">
+          <h3 class="font-light text-4xl -tracking-wider mt-0 mb-8 mx-0">
+            Jingjie Li <span class="text-3xl opacity-60">(李竞捷) </span>
+          </h3>
+
+        </div>
       }
       description={
-        <>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus{' '}
-          <a className="text-cyan-400 hover:underline" href="/">
-            malesuada
-          </a>{' '}
-          nisi tellus, non imperdiet nisi tempor at. Lorem ipsum dolor sit amet,{' '}
-          <a className="text-cyan-400 hover:underline" href="/">
-            consectetur
-          </a>{' '}
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.
-        </>
+          <h4 class="font-light mt-0 mb-4 mx-0">
+            I am a Lecturer/Asstistant Professor in the School of Informatics, University of Edinburgh and member of the Institute for Computing Systems Architecture.
+            nisi tellus, non imperdiet nisi tempor at. Lorem ipsum dolor sit amet,{' '}
+            <a className="text-cyan-400 hover:underline" href="/">
+              consectetur
+            </a>{' '}
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.
+          </h4>
       }
       avatar={
         <img
-          className="h-80 w-64"
-          src="/assets/images/avatar.svg"
+          className="h-60 w-50"
+          src="/assets/images/Headshot_Jingjie Li.png"
           alt="Avatar image"
           loading="lazy"
         />
       }
       socialButtons={
         <>
-          <a href="/">
+          <a href="https://twitter.com/j_j_li">
             <HeroSocial
-              src="/assets/images/twitter-icon.png"
+              src="/assets/images/icon-twitter.png"
               alt="Twitter icon"
             />
           </a>
           <a href="/">
             <HeroSocial
-              src="/assets/images/facebook-icon.png"
-              alt="Facebook icon"
+              src="/assets/images/cv-icon.png"
+              alt="CV icon"
             />
           </a>
           <a href="/">
             <HeroSocial
               src="/assets/images/linkedin-icon.png"
               alt="Linkedin icon"
-            />
-          </a>
-          <a href="/">
-            <HeroSocial
-              src="/assets/images/youtube-icon.png"
-              alt="Youtube icon"
             />
           </a>
         </>
