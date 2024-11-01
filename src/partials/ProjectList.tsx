5f9557350c611f417875efd5d48eb25b44202ebb
jingjieli95 <41805075+jingjieli95@users.noreply.github.com>
2024-11-01 16:29:29 +0000

    message

--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -18,23 +18,23 @@ const ProjectList = () => (
   >
     <div className="flex flex-col gap-6">
       <Project
-        name={<span class="font-light text-blue-900 font-xl">User-Centric Security and Privacy Control</span>}
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
+        name={<span class="font-light text-lime-300 font-xl">User-Centric Security and Privacy Control</span>}
+        description={<span class="text-sm text-slate-100">Security and privacy are "expensive" for cyber-physical systems that interact with users, which drawing rising concerns. 
+        Our research designs tools to make security and privacy practical and affordable to end-users, by preserving 
+        utilities and offering flexible and automated controls, in applications such as AR/VR technologies [USENIX Security21], mobile and IoT systems [ACM CCS19, MobiQuitous19]. </span>}
         img={{
-          src: '/assets/images/project-web-design.png',
-          alt: 'Project Web Design',
+          src: '/assets/images/privacy control_illustration.png',
+          alt: 'Privacy tool illustration',
         }}
 
       />
       <Project
-        name="Project 2"
+        name={<span class="font-light text-lime-300 font-xl">User-Centric Security and Privacy Control</span>}
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
         bibendum. Nunc non posuere consectetur, justo erat semper enim, non
         hendrerit dui odio id enim."
         link="/"
-        img={{ src: '/assets/images/project-fire.png', alt: 'Project Fire' }}
+        img={{ src: '/assets/images/human centric system_illustration.png', alt: 'Project Fire' }}
         category={
           <>
             <Tags color={ColorTags.VIOLET}>Next.js</Tags>
