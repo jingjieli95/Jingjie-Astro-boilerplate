208edd26835dfb42f81fa19e74de349aac79e4e1
jingjieli95 <41805075+jingjieli95@users.noreply.github.com>
2024-11-04 13:22:42 +0000

    message

---
+++ src/partials/Awards.tsx
@@ -0,0 +1,68 @@
+import {
+  ColorTags,
+  GradientText,
+  Project,
+  Section,
+  Tags,
+} from 'astro-boilerplate-components';
+
+const Awards = () => (
+  <Section
+    title={
+
+      <div>
+        <h3 class="font-light">
+          Selected <span class="text-blue-900">Awards and Honors</span>
+        </h3>
+      </div>
+
+
+    }
+    children={
+    <ul class="list-disc list-inside font-light text-base">
+      <li><span class="text-blue-900">Generative AI Laboratory Seedcorn Award</span>, 
+      University of Edinburgh <span class="italic">(2024)</span>
+      </li>
+      <li><span class="text-blue-900">CPS (Cyber-Physical Systems) Rising Star</span>, 
+      CPS-VO @ National Science Foundation <span class="italic">(2022)</span>
+      </li>
+      <li><span class="text-blue-900">Norton Labs Graduate Fellowship Finalist </span> 
+      <span class="italic">(2022)</span>
+      </li>
+      <li><span class="text-blue-900">Facebook Towards Trustworthy Products in AR, VR, and Smart Devices Award</span>  
+      <span class="italic">(2019 & 2021)</span>
+      </li>
+      <li><span class="text-blue-900">Qualcomm Innovation Fellowship Finalist </span> 
+      <span class="italic">(2019 & 2021)</span>
+      </li>
+        <li><span class="text-blue-900">UW–Madison Chancellor’s Opportunity Fellowship </span> 
+      <span class="italic">(2019 - 2021)</span>
+      </li>
+      <li><span class="text-blue-900">Foxconn “Smart Cities-Smart Futures” Competition Final Winner </span> 
+      < span class="italic">(2019)</span>
+      </li>
+      <li><span class="text-blue-900">ACM CHI Best Paper Award </span>  
+       <span class="italic">(2019)</span>
+      </li>
+      <li><span class="text-blue-900">ISLPED Low-Power Design Contest Award </span>  
+       <span class="italic">(2018)</span>
+      </li>
+      <li><span class="text-blue-900">A. Richard Newton Young Student Fellowship </span>   
+       <span class="italic">(2018)</span>
+      </li>
+      <li><span class="text-blue-900">ANU College of Engineering & Computer Science International Partnership Scholarship </span>   
+       <span class="italic">(2015 & 2016)</span>
+      </li>
+
+
+    </ul>
+    }
+
+
+  >
+
+  </Section>
+
+);
+
+export { Awards };
---
+++ src/partials/Services.tsx
@@ -0,0 +1,50 @@
+import {
+  ColorTags,
+  GradientText,
+  Project,
+  Section,
+  Tags,
+} from 'astro-boilerplate-components';
+
+const Services = () => (
+  <Section
+    title={
+
+      <div>
+        <h3 class="font-light">
+          Academic <span class="text-blue-900">Services</span>
+        </h3>
+      </div>
+
+
+    }
+    children={
+    <ul class="list-disc list-inside font-light text-base">
+      <li><span class="text-blue-900">Program Committee Member</span>
+         <ul class="list-disc list-inside font-light text-sm indent-12">
+            <li> ACM CCS (2023, 2024, & 2025) </li>
+            <li> USENIX Security (2025)</li>
+            <li> EuroUSEC (2024)</li>
+            <li> IEEE SafeThings (2023)</li>
+          </ul>
+      </li>
+      <li><span class="text-blue-900">Reviewer</span>
+         <ul class="list-disc list-inside font-light text-sm indent-12">
+            <li> I regularly accept review invitations for conferences and journals in computer and mobile systems 
+            (e.g., IEEE TC, IEEE TMC, IEEE Pervasive Computing), cybersecurity (e.g., IEEE S&P Magazine), and HCI (ACM CHI and ACM CSCW).</li>
+          </ul>
+      </li>
+
+
+    </ul>
+
+    }
+
+
+  >
+
+  </Section>
+
+);
+
+export { Services };
